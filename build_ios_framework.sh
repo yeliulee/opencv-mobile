64832b5f10d617fbb89ee93fdf7b7409291f6a19
yeliulee <yeliuleet@gmail.com>
2023-11-15 20:56:44 +0800

    fix

--- build_ios_framework.sh
+++ build_ios_framework.sh
@@ -10,30 +10,31 @@ export ENABLE_VISIBILITY=OFF
 
 
 
-cd $SCRIPT_DIR
+# cd $SCRIPT_DIR
 
-wget -q https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip -O opencv-$OPENCV_VERSION.zip
-unzip -q opencv-$OPENCV_VERSION.zip
-cd opencv-$OPENCV_VERSION
-echo -n > cmake/OpenCVFindLibsGrfmt.cmake
-rm -rf modules/gapi
-patch -p1 -i ../opencv-4.8.1-no-rtti.patch
-patch -p1 -i ../opencv-4.8.1-no-zlib.patch
-patch -p1 -i ../opencv-4.8.1-link-openmp.patch
-rm -rf modules/highgui
-cp -r ../highgui modules/
+# wget -q https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip -O opencv-$OPENCV_VERSION.zip
+# unzip -q opencv-$OPENCV_VERSION.zip
+# cd opencv-$OPENCV_VERSION
+# truncate -s 0 cmake/OpenCVFindLibsGrfmt.cmake
+# rm -rf modules/gapi
+# patch -p1 -i ../opencv-4.8.1-no-rtti.patch
+# patch -p1 -i ../opencv-4.8.1-no-zlib.patch
+# patch -p1 -i ../opencv-4.8.1-link-openmp.patch
+# rm -rf modules/highgui
+# cp -r ../highgui modules/
 
 cd $SCRIPT_DIR
 
 ## build
 
 cd opencv-$OPENCV_VERSION
+rm -rf build-arm64
 mkdir build-arm64 && cd build-arm64
 cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchains/ios.toolchain.cmake -DPLATFORM=OS -DARCHS="arm64" \
--DDEPLOYMENT_TARGET=$IOS_DEPLOYMENT_TARGET -DENABLE_BITCODE=$ENABLE_BITCODE -DENABLE_ARC=$ENABLE_ARC -DENABLE_VISIBILITY=$ENABLE_VISIBILITY \
--DCMAKE_C_FLAGS="-fno-rtti -fno-exceptions" -DCMAKE_CXX_FLAGS="-fno-rtti -fno-exceptions" \
--DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release `cat ../../opencv4_cmake_options.txt` -DBUILD_opencv_world=ON -DOPENCV_DISABLE_FILESYSTEM_SUPPORT=ON ..
-cmake --build . -j
+    -DDEPLOYMENT_TARGET=$IOS_DEPLOYMENT_TARGET -DENABLE_BITCODE=$ENABLE_BITCODE -DENABLE_ARC=$ENABLE_ARC -DENABLE_VISIBILITY=$ENABLE_VISIBILITY \
+    -DCMAKE_C_FLAGS="-fno-rtti -fno-exceptions" -DCMAKE_CXX_FLAGS="-fno-rtti -fno-exceptions" \
+    -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release `cat ../../opencv4_cmake_options.txt` -DBUILD_opencv_world=ON -DOPENCV_DISABLE_FILESYSTEM_SUPPORT=ON ..
+cmake --build . -j 8
 cmake --build . --target install
 
 ## package
